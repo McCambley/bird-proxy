e1497d0649511d2c437cb2bb2e5ce6809c4412ed
McCambley <jakemccamb16@gmail.com>
2022-03-09 22:24:41 -0500

    Dev: complete first instance of server

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const request = require('request');
+
+const app = express();
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  next();
+});
+
+app.get('/sounds/', (req, res) => {
+  request(
+    { url: `https://www.xeno-canto.org/api/2/recordings?query=${birdQuery}` },
+    (error, response, body) => {
+      if (error || response.statusCode !== 200) {
+        return res.status(500).json({ type: 'error', message: error.message });
+      }
+
+      res.json(JSON.parse(body));
+    }
+  );
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`listening on ${PORT}`));
